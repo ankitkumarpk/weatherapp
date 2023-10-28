b6666af0bafb7e9d6f675d0e67be26ab45aab888
ankitkumarpk <112842779+ankitkumarpk@users.noreply.github.com>
2023-10-28 14:58:59 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "YOUR-API-KEY-HERE"; // API key for OpenWeatherMap API
+const API_KEY = "b4befb50baf2b69f7465d6d9d3a0f652"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
